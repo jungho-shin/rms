6102018a5ad49278d7bed4ba468fb9dadc3f44e9
jhshin <jhshin@jhshin-PC>
2015-12-31 09:13:33 +0900

    [publishing] Modify new_account page for company mode.

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -135,6 +135,164 @@ var Page = {
 	}
 }
 
+var Api = {
+	
+	"apiPath" : "http://127.0.0.1:3000",
+	"apiJsonPPath" : "http://127.0.0.1:3000/jsonp",
+	
+	"guestToken" : "c72697466effd0db4ac78592b40c323607bguest",
+	
+	_getHeader : function( customHeader ){
+		
+		var httpHeaders = {};
+    	if(customHeader){
+    		httpHeaders = customHeader;
+    		
+    	} else {
+    		httpHeaders = {
+    	    		//,"token": token
+            }
+	    	if ( User.token && User.token != "" ) {
+	    		httpHeaders["Token"] = User.token;
+	    	} else {
+	    		httpHeaders["Token"] = this.guestToken;
+	    	}
+    	}
+    	
+    	return httpHeaders;
+		
+	},
+	
+	_call : function (method, url, data, success_callback, fail_callback , customHeader) {
+		
+    	console.log(method + " : " + url );
+    	var sendingData = data;
+    	if( method.toUpperCase() == "POST") sendingData = JSON.stringify(data);
+    	
+    	
+    	// ie10+ & modern browsers have "window.atob"
+        if ( !window.atob ) { //old browser( old IE )
+        	
+        	if(url.indexOf("http://", 0)>-1){
+        	} else {
+        		url = Api.apiJsonPPath + '/' + method.toLowerCase() + '/' + url.replace(/^\//g, '');
+        		
+        		url += "?json=" + JSON.stringify(data) //jsonData; //url += $.param(data);
+        		
+        		if(customHeader){
+        		} else {
+        			if ( User.token && User.token != "" ) {
+        				url += "&token=" + User.token;
+        	    	} else {
+        	    		url += "&token=" + this.guestToken;
+        	    	}
+        		}
+        	}
+
+			/*if (arguments.length == 2) {
+				success_callback = data;
+				data = null;
+			}*/
+            
+            //loadJsCssFile( url , "js" , success_callback );
+            $.getScript( url , function( data, textStatus, jqxhr ) {
+            	
+            	//document.title = 'MyFitSize';
+            	console.log( textStatus ); 
+            	console.log( jqxhr.status ); 
+            	console.log( "Load was performed." );
+            	
+            	//loaded script start with "var jsonpRes = ..." so, 
+            	success_callback( jsonpRes ); 
+            	
+            });
+            
+            return;
+        }
+        
+        
+        //CASE of modern browser : cors ajax
+        
+        var httpHeaders = this._getHeader(customHeader);
+        
+        if(url.indexOf("http://", 0)>-1){
+    		
+    	} else {
+    		url = Api.apiPath + '/' + url.replace(/^\//g, '');
+    	}
+
+        /*if (arguments.length == 2) {
+            success_callback = data;
+            data = null;
+        }*/
+
+        $.ajax(
+            {
+                url: url,
+                type: method.toUpperCase() ,
+                //processData: false,
+                data: sendingData,
+                contentType: 'application/json; charset=UTF-8',
+                dataType: 'json',
+            	headers: httpHeaders,
+	            beforeSend: function (request, settings) {
+//		              // modal background
+//		              var $back = $('<div class="modal-background ajax-modal-background"></div>');
+//		              $('.modal-background.ajax-modal-background').remove();
+//		              $('body').prepend($back);
+//		              $back.hide().css('z-index', new Date().getTime()).fadeIn(500);
+	            }
+            }).done(function (result, textStatus, jqXHR) {
+//					if (!res.status)
+//					{
+//						alertNotice(res.message);
+//						return;
+//					}
+                success_callback(result, textStatus, jqXHR);
+            }).fail(function (jx, textStatus, errorThrown) {
+                try {
+                    var res = JSON.parse(jx.responseText);
+                    var status = JSON.parse(jx.status);
+                }
+                catch (ex) {
+                    if (console) console.log(jx.description);
+                    return;
+                }
+
+                if (fail_callback) {
+                	if(status == 498) {
+                		Loader.hide();
+                		Popup.alertCallback(res, function() {
+                			location = "#login";
+                		});
+                	} else if(status == 401) {
+                		Loader.hide();
+                		Popup.alert(res);
+                	} else {
+                        fail_callback(res);
+                	}
+                }
+                else {
+                    alertWarning(res.message);
+                }
+            }).always(function () {
+                $('.modal-background.ajax-modal-background').remove();
+            });
+    },
+    
+	
+    get: function (url, data, success_callback, fail_callback, customHeader) {
+    	
+    	this._call("get", url, data, success_callback, fail_callback, customHeader);
+    	
+    },
+
+    post: function (url, data, success_callback, fail_callback , customHeader) {
+
+    	this._call("post", url, data, success_callback, fail_callback, customHeader);
+    }
+};
+
 $(document).ready(function() {
 
 	/*
--- www/assets/js/page/new_account.js
+++ www/assets/js/page/new_account.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+
+	$("#create").on("click", function() {
+		Api.post("/", {}, function(res) {
+			console.log(">>>>> success");
+		}, function(res) {
+			console.log(">>>>> fail");
+		});
+	});
+
 	$("#cancel").on("click", function() {
 		goBack();
 	});
 });
+
+function isValid() {
+	
+}
