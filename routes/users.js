11e5ab7c79ca3717247963af85d611054de3607a
jhshin <jhshin@jhshin-PC>
2016-01-21 11:20:41 +0900

    디버깅을 위한 tracer 모듈 추가.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var conf = require('./../config');
+var logger = require('tracer')[conf.log.strategy](conf.log.setting.log);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  logger.log("GET user listing");
   res.send('respond with a resource');
 });
 
